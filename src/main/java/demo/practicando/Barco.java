0cf56574599e7a17a2ef91ac34f8b31989e2585c
Felipe-hen30 <henao-30@outlook.com>
2024-09-11 19:44:49 +0200

    Barco con metodos modificadores

--- src/main/java/demo/practicando/Barco.java
+++ src/main/java/demo/practicando/Barco.java
@@ -2,8 +2,34 @@ package demo.practicando;
 
 public class Barco {
 
-	String nombre = "titanic";
-	int inagurado = 1912;
-	Double velocidad = 38.9;
+	private String nombre = "titanic";
+	private int inagurado = 1912;
+	private Double velocidad = 38.9;
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+
+	}
+
+	public void setInaguradoo(int inagurado) {
+		this.inagurado = inagurado;
+	}
+
+	public int getInagurado() {
+		return this.inagurado;
+
+	}
+
+	public void setVelocidad(Double velocidad) {
+		this.velocidad = velocidad;
+		
+	}
+
+	public Double getVelocidad() {
+		return this.velocidad;
+	}
 }
