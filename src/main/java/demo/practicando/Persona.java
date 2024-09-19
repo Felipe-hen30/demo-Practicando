261f39c82ebbb592dc64ddc62d80ff5d21847475
Felipe-hen30 <henao-30@outlook.com>
2024-09-19 22:04:03 +0200

    modificando a la persona

--- src/main/java/demo/practicando/Persona.java
+++ src/main/java/demo/practicando/Persona.java
@@ -2,10 +2,33 @@ package demo.practicando;
 
 public class Persona {
 
-	String cualidad = "bondadosa";
-	int propiedades = 4;
-	Double patrimonio = 369800.56;
+	String cualidad;
+	int propiedades;
+	Double patrimonio;
 
-	
-	
+	public void setCualidad(String cualidad) {
+		this.cualidad = cualidad;
+
+	}
+
+	public String getCualidad() {
+		return this.cualidad = cualidad;
+	}
+
+	public void setPropiedades(int propiedades) {
+		this.propiedades = propiedades;
+	}
+
+	public int getPropiedades() {
+		return this.propiedades;
+	}
+
+	public void setPatrimonio(Double patrimonio) {
+		this.patrimonio = patrimonio;
+
+	}
+
+	public Double getPatrimonio() {
+		return this.patrimonio;
+	}
 }
