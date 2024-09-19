b1b091f6a8f1348cf63be236fa885b789e65dc4c
Felipe-hen30 <henao-30@outlook.com>
2024-09-19 21:51:44 +0200

    modificando al gato

--- src/main/java/demo/practicando/Gato.java
+++ src/main/java/demo/practicando/Gato.java
@@ -2,11 +2,29 @@ package demo.practicando;
 
 public class Gato {
 
-	String nombre = "alveiro";
-	int vidasRestantes = 1;
-	Double testFlexibilidad = -16.8;
+	String nombre;
+	int vidasRestantes;
+	Double testFlexibilidad;
 	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+		
+	}
 	
+	public String getNombre( ) {
+		return this.nombre;
+	}
 	
-	
+	public void setVidasRestantes(int vidasRestantes) {
+		this.vidasRestantes = vidasRestantes;
+	}
+	public int getVidasRestantes( ) {
+		return this.vidasRestantes;
+	}
+	public void setTestFlexibilidad(Double tesFlexibilidad) {
+		this.testFlexibilidad = testFlexibilidad; 
+	}
+	public Double getTestFlexibilidad() {
+		return this.testFlexibilidad;
+	}
 }
